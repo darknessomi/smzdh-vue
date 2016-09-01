92f41846e1cf1800cdc89fbdf7b712ecc3d5f409
omi <4399.omi@gmail.com>
2016-09-01 12:13:46 +0800

    add email

--- src/views/signin.vue
+++ src/views/signin.vue
@@ -7,26 +7,26 @@
         margin-top: 100px;
     }
     input{
-        width: 20%;
+        width: 30%;
         height: 30px;
         line-height: 30px;
         border: 1px solid #ccc;
         text-align: center;
         font-size: 20px;
         margin-top: 20px;
-        margin-right: 40%;
-        margin-left: 40%;
+        margin-right: 35%;
+        margin-left: 35%;
     }
     div button{
-        width: 20%;
+        width: 30%;
         height: 30px;
         line-height: 20px;
         background-color: #ccc;
         text-align: center;
         font-size: 20px;
         margin-top: 20px;
-        margin-right: 40%;
-        margin-left: 40%;
+        margin-right: 35%;
+        margin-left: 35%;
     }
 </style>
 <template>
@@ -38,7 +38,7 @@
     <alert :show.sync="showTop" placement="top" duration="3000" type="danger" width="400px" dismissable>
       <span class="icon-info-circled alert-icon-float-left"></span>
       <strong>登陆错误</strong>
-      <p>请检查用户名密码是否正确</p>
+      <p>{{ message }}</p>
     </alert>
     <div><button v-on:click="signin">登陆</button></div>
 </template>
@@ -54,7 +54,8 @@
             return {
                 username: '',
                 password: '',
-                showTop: false
+                showTop: false,
+                message: ''
             }
         },
         ready () {
@@ -65,24 +66,29 @@
         },
         methods: {
             signin: function (event) {
-                let useinfo = {
-                    username: this.username,
-                    password: md5(this.password)
-                };
-                this.$http.post('/api/signin', useinfo).then((response) => {
-                    console.log(response)
-                    console.log(this.show)
-                    if (response.status == 200 && response.ok) {
-                        alert("登陆成功")
-                    } else {
+                if (this.username.length > 0 && this.password.length > 0) {
+                    let useinfo = {
+                        username: this.username,
+                        password: md5(this.password)
+                    };
+                    this.$http.post('/api/signin', useinfo).then((response) => {
+                        console.log(response)
+                        if (response.status == 200 && response.ok) {
+                            window.alert("登陆成功")
+                        } else {
+                            this.message = '请检查用户名密码是否正确'
+                            this.showTop = true
+                        }
+                    }, (response) => {
+                        console.log(response)
+                        // error callback
+                        this.message = JSON.parse(response.body).error
                         this.showTop = true
-                    }
-                }, (response) => {
-                    console.log(response)
-                    // error callback
+                    });
+                } else {
+                    this.message = '请检查用户名密码是否正确'
                     this.showTop = true
-                });
-
+                }
             }
         }
     }
--- src/views/signup.vue
+++ src/views/signup.vue
@@ -7,44 +7,57 @@
         margin-top: 100px;
     }
     input{
-        width: 20%;
+        width: 30%;
         height: 30px;
         line-height: 30px;
         border: 1px solid #ccc;
         text-align: center;
         font-size: 20px;
         margin-top: 20px;
-        margin-right: 40%;
-        margin-left: 40%;
+        margin-right: 35%;
+        margin-left: 35%;
     }
     div button{
-        width: 20%;
+        width: 30%;
         height: 30px;
         line-height: 20px;
         background-color: #ccc;
         text-align: center;
         font-size: 20px;
         margin-top: 20px;
-        margin-right: 40%;
-        margin-left: 40%;
+        margin-right: 35%;
+        margin-left: 35%;
     }
 </style>
 <template>
     <h1>
         注册
     </h1>
+    <input type="email" v-model="email" placeholder="email">
     <input type="text" v-model="username" placeholder="username">
     <input type="password" v-model="password" placeholder="password">
+    <alert :show.sync="showTop" placement="top" duration="3000" type="danger" width="400px" dismissable>
+      <span class="icon-info-circled alert-icon-float-left"></span>
+      <strong>注册错误</strong>
+      <p>{{ message }}</p>
+    </alert>
     <div><button v-on:click="register">注册</button></div>
 </template>
 <script>
     import md5 from 'blueimp-md5';
+    import { alert } from 'vue-strap'
 
     export default {
+        components: {
+            alert,
+        },
         data () {
             return {
                 username: '',
-                password: ''
+                email: '',
+                password: '',
+                showTop: false,
+                message: ''
             }
         },
         ready () {
@@ -55,23 +68,27 @@
         },
         methods: {
             register: function (event) {
-                let useinfo = {
-                    username: this.username,
-                    password: md5(this.password)
-                };
-                this.$http.post('/api/signup', useinfo).then((response) => {
-                    console.log(response)
-                    if (response.status == 200 && response.ok) {
-                        alert("注册成功")
-                    } else {
-                        alert("注册错误")
-                    }
-                }, (response) => {
-                    console.log(response)
-                    // error callback
-                    alert("注册错误")
-                });
-
+                if (this.email.length > 0 && this.username.length > 0 && this.password.length > 0) {
+                    let useinfo = {
+                        email: this.email,
+                        username: this.username,
+                        password: md5(this.password)
+                    };
+                    this.$http.post('/api/signup', useinfo).then((response) => {
+                        console.log(response)
+                        if (response.status == 200 && response.ok) {
+                            window.alert("注册成功")
+                        }
+                    }, (response) => {
+                        console.log(response)
+                        // error callback
+                        this.message = JSON.parse(response.body).error
+                        this.showTop = true
+                    });
+                } else {
+                    this.message = '请检查输入信息是否正确'
+                    this.showTop = true
+                }
             }
         }
     }
